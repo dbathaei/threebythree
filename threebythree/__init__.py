4e2c0e0e169afa014720389af0c4969c744f0229
dbathaei <dbathaei@gmail.com>
2023-06-19 05:34:21 -0700

    changes for meeting

--- threebythree/__init__.py
+++ threebythree/__init__.py
@@ -16,7 +16,7 @@ class C(BaseConstants):
     GREEN_ROLE = 'Green'
 
     # For now we have a fixed number of rounds.
-    NUM_ROUNDS = 5
+    NUM_ROUNDS = 20
 
 
     # Choices that an individual player sees. A,B,C is what they see on the screen and 0,1,2 is the input we take from them.
@@ -38,18 +38,134 @@ class C(BaseConstants):
     '''
     for round in range(NUM_ROUNDS):
         PAYOFF_DICT_LIST[f"payoff_round_{round + 1}"] = {}
+        PAYOFF_DICT_LIST[f"payoff_round_{round + 1}"]["key_identifier"] = f"random_round_{ round + 1 }"
         for i in range(3):
             for j in range(3):
                 for k in range(2):
                     PAYOFF_DICT_LIST[f"payoff_round_{round + 1}"][f"C{i}{j}_{k}"] = 10 * r.randint(1,9)
 
-
     # randomly decides which round is the one that pays the players. Is between 1 and NUM_ROUNDS inclusive.
     PAYOFF_ROUND = r.randint(1, NUM_ROUNDS)
+    WHICHGAME_LIST = [1,2,3,4,5,1,2,3,4,5]
+    r.shuffle(WHICHGAME_LIST)
+    for i in range(5):
+        WHICHGAME_LIST.append(i+1)
+    
+    COINTOSS = [1,1,1,1,1,1,1,1,1,1,0,0,0,0,0]
+    r.shuffle(COINTOSS)
+
 
 class Subsession(BaseSubsession):
-    pass
-
+    FIXED_DICT_LIST = {
+    "fixed_round_1": {
+                        "key_identifier": "fixed_round_1",
+                        "C00_0": 80,
+                        "C00_1": 90,
+                        "C01_0": 20,
+                        "C01_1": 60,
+                        "C02_0": 80,
+                        "C02_1": 80,
+                        "C10_0": 40,
+                        "C10_1": 80,
+                        "C11_0": 40,
+                        "C11_1": 20,
+                        "C12_0": 70,
+                        "C12_1": 70,
+                        "C20_0": 50,
+                        "C20_1": 60,
+                        "C21_0": 60,
+                        "C21_1": 10,
+                        "C22_0": 40,
+                        "C22_1": 60
+                    },
+    "fixed_round_2": {
+                        "key_identifier": "fixed_round_2",
+                        "C00_0": 80,
+                        "C00_1": 60,
+                        "C01_0": 10,
+                        "C01_1": 60,
+                        "C02_0": 40,
+                        "C02_1": 80,
+                        "C10_0": 10,
+                        "C10_1": 10,
+                        "C11_0": 60,
+                        "C11_1": 40,
+                        "C12_0": 90,
+                        "C12_1": 10,
+                        "C20_0": 80,
+                        "C20_1": 50,
+                        "C21_0": 50,
+                        "C21_1": 10,
+                        "C22_0": 20,
+                        "C22_1": 40
+                    },
+    "fixed_round_3": {
+                        "key_identifier": "fixed_round_3",
+                        "C00_0": 90,
+                        "C00_1": 40,
+                        "C01_0": 30,
+                        "C01_1": 80,
+                        "C02_0": 70,
+                        "C02_1": 20,
+                        "C10_0": 90,
+                        "C10_1": 80,
+                        "C11_0": 80,
+                        "C11_1": 60,
+                        "C12_0": 70,
+                        "C12_1": 10,
+                        "C20_0": 90,
+                        "C20_1": 10,
+                        "C21_0": 30,
+                        "C21_1": 40,
+                        "C22_0": 20,
+                        "C22_1": 80
+                    },
+    "fixed_round_4": {
+                        "key_identifier": "fixed_round_4",
+                        "C00_0": 30,
+                        "C00_1": 50,
+                        "C01_0": 90,
+                        "C01_1": 20,
+                        "C02_0": 60,
+                        "C02_1": 90,
+                        "C10_0": 80,
+                        "C10_1": 90,
+                        "C11_0": 10,
+                        "C11_1": 40,
+                        "C12_0": 40,
+                        "C12_1": 90,
+                        "C20_0": 90,
+                        "C20_1": 90,
+                        "C21_0": 90,
+                        "C21_1": 60,
+                        "C22_0": 20,
+                        "C22_1": 30
+                    },
+    "fixed_round_5": {
+                        "key_identifier": "fixed_round_5",
+                        "C00_0": 70,
+                        "C00_1": 90,
+                        "C01_0": 70,
+                        "C01_1": 10,
+                        "C02_0": 30,
+                        "C02_1": 90,
+                        "C10_0": 70,
+                        "C10_1": 80,
+                        "C11_0": 90,
+                        "C11_1": 50,
+                        "C12_0": 70,
+                        "C12_1": 10,
+                        "C20_0": 50,
+                        "C20_1": 30,
+                        "C21_0": 40,
+                        "C21_1": 90,
+                        "C22_0": 20,
+                        "C22_1": 90
+                    }
+}
+    @staticmethod
+    def creating_session(subsession):
+        subsession.group_randomly()
 
 class Group(BaseGroup):
     pass
@@ -81,13 +197,32 @@ class DecisionPage(Page):
     # introduces the matrix "payoff_round_{player.round_number}" for each round.
     @staticmethod
     def vars_for_template(player: Player):
-        for round in range(C.NUM_ROUNDS):
-            if player.round_number == round + 1:
-                payoff_for_this_round = C.PAYOFF_DICT_LIST[f"payoff_round_{round + 1}"]
-            else:
-                pass
+        CoinToss = C.COINTOSS[player.round_number-1]
+        WhichGame = C.WHICHGAME_LIST[player.round_number-1]
+        if CoinToss == 0 and len(Subsession.FIXED_DICT_LIST) != 0:
+            for Game in Subsession.FIXED_DICT_LIST:
+                if Subsession.FIXED_DICT_LIST[f"fixed_round_{WhichGame}"] == Subsession.FIXED_DICT_LIST[Game]:
+                    payoff_for_this_round = Subsession.FIXED_DICT_LIST[f"fixed_round_{WhichGame}"]
+                else:
+                    pass
+        elif 15 - player.round_number == len(Subsession.FIXED_DICT_LIST):
+            for Game in Subsession.FIXED_DICT_LIST:
+                if Subsession.FIXED_DICT_LIST[f"fixed_round_{WhichGame}"] == Subsession.FIXED_DICT_LIST[Game]:
+                    payoff_for_this_round = Subsession.FIXED_DICT_LIST[f"fixed_round_{WhichGame}"]
+                else:
+                    pass
+        else:
+            for round in range(C.NUM_ROUNDS):
+                if player.round_number == round + 1:
+                    payoff_for_this_round = C.PAYOFF_DICT_LIST[f"payoff_round_{round + 1}"]
+                else:
+                    pass
         return payoff_for_this_round
-
+    
+    @staticmethod
+    def CoinTossForTemplate(player: Player):
+        CoinToss = C.COINTOSS[player.round_number-1]
+        return CoinToss
 
     # Failsafe method that ensures a session continues even if we push a player to the next round.
     @staticmethod 
